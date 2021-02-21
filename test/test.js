319552d3570169562cf5e5c4e6b7041891c8c0c4
marieblessed <57671889+marieblessed@users.noreply.github.com>
2021-02-21 21:52:29 +0300

    first travis commit

---
+++ test/test.js
@@ -0,0 +1,15 @@
+const supertest = require('supertest');
+
+var server = supetest.agent('http://localhos:3000');
+
+describe('Sampleunit test', () => {
+  it('should return home page', (done) => {
+    server
+      .get('/')
+      .expect('Content-type', /text/)
+      .expect(200)
+      .end(function (err, res) {
+        done();
+      });
+  });
+});
