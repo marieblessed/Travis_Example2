3e07e9a6d24ea6b61b6dd6b8736cf19cd5e4a3a5
marieblessed <57671889+marieblessed@users.noreply.github.com>
2021-02-25 17:28:32 +0300

    Update;- API done

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const express = require('express');
-
+const port = 3000;
 const app = express();
-
-app.get('/', (req, res) =>{
-    res.send('Working');
+const admin = require("firebase-admin");
+const serviceAccount = require("./phonebook.js.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
 });
 
-app.listen(3000)
+
+app.use(express.json());
+
+app.post('/add-phone-contacts', async(req, res) => {
+    try{
+        const contacts = req.body;
+        await admin.firestore().collection('phonecontacts').add(contacts);
+        res.status(200).send({message: "Stored Contatct"});
+    }catch(e){
+        console.log(e);
+        res.status(500).send({message: e});
+    }
+});
+
+app.get('/get-contacts', async (req, res) => {
+    try{
+        const contacts = await admin.firestore().collection('phonecontacts').get();
+        const results = [];
+        contacts.forEach(contact => {
+           results.push(contact.data());
+        });
+        res.status(200).send({message: results});
+    }catch(e){
+        res.status(500).send({message: e});
+    }
+});
+
+app.listen(port, () => console.log(`Sever running on port ${port}`));
 
 
-
-
-//module.exports = test.js;
